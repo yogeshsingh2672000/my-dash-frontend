f3f97b2c946d02ab1cde546581944dce1749303c
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-05-19 23:59:46 +0530

    installed sass

--- README.md
+++ README.md
@@ -4,12 +4,14 @@
 
 ###### step-1 npx create-react-app my-dash-frontend
 
-###### step-2 fork the repo
+###### step-2 npm install sass
 
-###### step-3 clone my repo
+###### step-3 fork the repo
 
-###### step-4 replace all your my-dash-frontend files except "node module" folder
+###### step-4 clone my repo
 
-###### step-5 npm start
+###### step-5 replace all your my-dash-frontend files except "node module" folder
+
+###### step-6 npm start
 
 ## Happy Coding
--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -9,7 +9,52 @@ class Home extends Component {
         <div className="container__image">
           <img src={Background} alt="background image" />
         </div>
-        <div className="container__form"></div>
+        <div className="container__form">
+          <form action="#" method="post">
+            <table>
+              <tr>
+                <td className="contianer__form__tittle">Your Email Address</td>
+              </tr>
+              <tr>
+                <td>
+                  <input type="email" name="email" />
+                </td>
+              </tr>
+              <tr>
+                <td className="contianer__form__tittle">Your Password</td>
+              </tr>
+              <tr>
+                <td>
+                  <input type="password" name="pass" />
+                </td>
+              </tr>
+              <tr>
+                <td className="contianer__form__tittle">
+                  Confirm your Password
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <input type="password" name="pass" />
+                </td>
+              </tr>
+              <tr>
+                <td className="contianer__form__tittle">Your Full Name</td>
+              </tr>
+              <tr>
+                <input type="text" name="name" />
+              </tr>
+              <tr>
+                <td className="contianer__form__tittle">Your phone number</td>
+              </tr>
+              <tr>
+                <td>
+                  <input type="tel" name="mobile" />
+                </td>
+              </tr>
+            </table>
+          </form>
+        </div>
       </div>
     );
   }
