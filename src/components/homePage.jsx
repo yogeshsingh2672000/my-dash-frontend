56d2757dbba85193788c19aa2b30748ee23657e8
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-05-20 00:32:01 +0530

    image and form added

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Background from "./background.png";
+import scss from "./homePage.scss";
 
 class Home extends Component {
   // state = {  }
@@ -7,52 +8,80 @@ class Home extends Component {
     return (
       <div className="container">
         <div className="container__image">
-          <img src={Background} alt="background image" />
+          <img
+            className="container__image__background"
+            src={Background}
+            alt="background image"
+          />
         </div>
         <div className="container__form">
           <form action="#" method="post">
-            <table>
+            <h2 className="container__form__heading">Create an account</h2>
+            <table className="container__form__table">
               <tr>
-                <td className="contianer__form__tittle">Your Email Address</td>
+                <td className="container__form__tittle">Your Email Address</td>
               </tr>
               <tr>
                 <td>
-                  <input type="email" name="email" />
+                  <input
+                    className="container__form__input"
+                    type="email"
+                    name="email"
+                  />
                 </td>
               </tr>
               <tr>
-                <td className="contianer__form__tittle">Your Password</td>
+                <td className="container__form__tittle">Your Password</td>
               </tr>
               <tr>
                 <td>
-                  <input type="password" name="pass" />
+                  <input
+                    className="container__form__input"
+                    type="password"
+                    name="pass"
+                  />
                 </td>
               </tr>
               <tr>
-                <td className="contianer__form__tittle">
+                <td className="container__form__tittle">
                   Confirm your Password
                 </td>
               </tr>
               <tr>
                 <td>
-                  <input type="password" name="pass" />
+                  <input
+                    className="container__form__input"
+                    type="password"
+                    name="pass"
+                  />
                 </td>
               </tr>
               <tr>
-                <td className="contianer__form__tittle">Your Full Name</td>
+                <td className="container__form__tittle">Your Full Name</td>
               </tr>
               <tr>
-                <input type="text" name="name" />
+                <input
+                  className="container__form__input"
+                  type="text"
+                  name="name"
+                />
               </tr>
               <tr>
-                <td className="contianer__form__tittle">Your phone number</td>
+                <td className="container__form__tittle">Your phone number</td>
               </tr>
               <tr>
                 <td>
-                  <input type="tel" name="mobile" />
+                  <input
+                    className="container__form__input"
+                    type="tel"
+                    name="mobile"
+                  />
                 </td>
               </tr>
             </table>
+            <button className="container__form__button" type="submit">
+              Create an account
+            </button>
           </form>
         </div>
       </div>
